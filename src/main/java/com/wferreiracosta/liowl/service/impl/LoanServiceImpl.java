4e31087bfbefe3df7e92723321422054eae5bc43
Wesley Costa <wesferrcosta@gmail.com>
2021-01-09 21:41:50 -0300

    Criado LoanServiceImpl

---
+++ src/main/java/com/wferreiracosta/liowl/service/impl/LoanServiceImpl.java
@@ -0,0 +1,16 @@
+package com.wferreiracosta.liowl.service.impl;
+
+import com.wferreiracosta.liowl.model.entity.Loan;
+import com.wferreiracosta.liowl.service.LoanService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoanServiceImpl implements LoanService {
+
+    @Override
+    public Loan save(Loan any) {
+        return null;
+    }
+    
+}
