3e24dbae1f4e7af4d350a91a655a3db7442b148d
Wesley Costa <wesferrcosta@gmail.com>
2021-01-17 12:25:19 -0300

    Implementado Swagger

---
+++ src/main/java/com/wferreiracosta/liowl/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.wferreiracosta.liowl.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+    
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.wferreiracosta.liowl.api.resource"))
+            .paths(PathSelectors.any())
+            .build()
+            .apiInfo(this.apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+            .title("Liowl API")
+            .description("API de controle de aluguel de livros")
+            .version("1.0")
+            .contact(this.contact())
+            .build();
+    }
+
+    private Contact contact(){
+        return new Contact("Wesley Costa",
+                        "https://github.com/wferreiracosta",
+                        "wesferrcosta@gmail.com");
+    }
+
+}
